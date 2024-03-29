2621caddb865ddb0b797a39a5945cacf688c2eb0
shodikin-amin <shodikin.amin.qwerty@gmail.com>
2024-03-30 03:02:15 +0800

    ganti variabel

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const users = [
+let users = [
     { email: "user1@gmail.com", password: "password1", role: "admin" },
     { email: "user2@gmail.com", password: "password2", role: "user" }
 ];
 
 // Function to validate login
 function signIn() {
-    const emailInput = document.getElementById("email").value;
-    const passwordInput = document.getElementById("password").value;
+    let emailInput = document.getElementById("email").value;
+    let passwordInput = document.getElementById("password").value;
 
-    const rightUser = users.find(u => u.email === emailInput && u.password === passwordInput);
+    let User = users.find(u => u.email === emailInput && u.password === passwordInput);
 
-    if (rightUser) {
+    if (User) {
         // Update login status
-        document.getElementById("login-status").textContent = `Welcome, ${rightUser.email}, Sebagai (${rightUser.role})`;
+        document.getElementById("login-status").textContent = `Welcome, ${User.email}, Sebagai (${User.role})`;
         document.getElementById("login-status").style.color = "green";
 
     } else {
